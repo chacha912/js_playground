c284a0e13f45b6bc0a596f9e1032b08ab71030d6
chacha912 <dkwkdnwk912@gmail.com>
2021-06-03 18:41:14 +0900

    feat: 삽입정렬 구현 - for문 2번 & swap 이용

--- sort/insertion_sort.js
+++ sort/insertion_sort.js
@@ -2,15 +2,19 @@
 // 단, 어떠한 빌트인 함수도 사용하지 않고 for 문을 사용하여 구현하여야 한다.
 
 function insertionSort(array) {
+  const swap = (array, index1, index2) => {
+    const temp = array[index1];
+    array[index1] = array[index2];
+    array[index2] = temp;
+  };
+
   for (let i = 1; i < array.length; i++) {
-    let j = i - 1;
-    const key = array[i];
-    while (j >= 0 && array[j] > key) {
-      array[j + 1] = array[j];
-      j -= 1;
+    for (let j = i; j > 0; j--) {
+      if (array[j] >= array[j - 1]) break;
+      swap(array, j, j - 1);
     }
-    array[j + 1] = key;
   }
+
   return array;
 }
 
